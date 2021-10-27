bee6fed6420f9d5755ad32bb5ea91b529e0df0da
Tony Adams <notkelloggstony@gmail.com>
2021-10-26 20:18:24 -0400

    Created app, added routing, installed Material Design Icons, began early layout and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import logo from './logo.svg';
+import Icon from '@mdi/react';
+import { mdiGithub } from '@mdi/js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <nav id="navigation" className="noselect">
+          <img src={logo} className="app-logo" alt="logo" />
+          <ul id="navigation-links" className="navigation-list">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <ul id="navigation-icons" className="navigation-list">
+            <li title="View on GitHub">
+              <a href="https://github.com/notkelloggstony/ta-studio" target="_blank" rel="noreferrer">
+                <Icon className=".navigation-icon" path={mdiGithub} size="24px" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+    </Switch>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                <p>Home</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
